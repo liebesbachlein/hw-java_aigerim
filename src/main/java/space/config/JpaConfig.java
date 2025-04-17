55f6299b67d4197b796665c30c60cc49eacf9fde
aigerim tuishieva <gerardinearmstrong@gmail.com>
2025-04-17 19:44:43 +0500

    build imitation of migrations

--- src/main/java/space/config/JpaConfig.java
+++ src/main/java/space/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package space.config;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -9,6 +10,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import space.entity.Space;
+import space.entity.SpaceType;
+import space.entity.User;
+import space.service.auth.EncryptionService;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -36,7 +41,7 @@ public class JpaConfig {
 
         Properties jpaProperties = new Properties();
         jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        jpaProperties.put("hibernate.hbm2ddl.auto", "update");
+        jpaProperties.put("hibernate.hbm2ddl.auto", "create-drop");
         jpaProperties.put("hibernate.show_sql", "true");
         jpaProperties.put("hibernate.format_sql", "true");
 
@@ -47,8 +52,70 @@ public class JpaConfig {
 
     @Bean
     public EntityManagerFactory emf() {
-        return entityManagerFactoryBean().getNativeEntityManagerFactory();
+        EntityManagerFactory emf = entityManagerFactoryBean().getNativeEntityManagerFactory();
+        runMigrations(emf.createEntityManager());
+        return emf;
     }
 
+    private void runMigrations(EntityManager em) {
+        em.getTransaction().begin();
+        em.persist(new User(0,
+                "Admin1",
+                "admin@gmail.com",
+                EncryptionService.encode("12345"),
+                User.Role.ADMIN_ROLE));
+        User user = new User(
+                0,
+                "John Wick",
+                "johnwick@gmail.com",
+                EncryptionService.encode("12345"),
+                User.Role.USER_ROLE);
+        em.persist(user);
+        SpaceType type1 = new SpaceType(0,
+                "Private Room");
+        SpaceType type2 = new SpaceType(0,
+                "Conference");
+        SpaceType type3 = new SpaceType(0,
+                "Shared Workplace");
+        SpaceType type4 = new SpaceType(0,
+                "Cubicles");
+        SpaceType type5 = new SpaceType(0,
+                "Chill Zone");
 
+        em.persist(type1);
+        em.persist(type2);
+        em.persist(type3);
+        em.persist(type4);
+        em.persist(type5);
+
+        Space space1 = new Space(0,
+                "California Vibes",
+                1000,
+                type5);
+        Space space2 = new Space(0,
+                "Apple Inc",
+                50000,
+                type2);
+        Space space3 = new Space(0,
+                "Silicon Valley",
+                15000,
+                type4);
+        Space space4 = new Space(0,
+                "Typical Workplace",
+                20050,
+                type5);
+        Space space5 = new Space(0,
+                "Highway to Paradise",
+                800,
+                type1);
+
+        em.persist(space1);
+        em.persist(space2);
+        em.persist(space3);
+        em.persist(space4);
+        em.persist(space5);
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
+    }
 }
